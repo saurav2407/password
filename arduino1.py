0d51a56cb489a4b6b1c973bdd7d0f18daa7be37b
saurav2407 <135573682+saurav2407@users.noreply.github.com>
2023-08-06 04:04:41 +0530

    Add files via upload

---
+++ arduino1.py
@@ -0,0 +1,32 @@
+import serial.tools.list_ports
+ports = serial.tools.list_ports.comports()
+serialInst = serial.Serial()
+
+portsList = []
+
+for onePort in ports:
+    portsList.append(str(onePort))
+    print(str(onePort))
+    
+val = input("select port : COM")
+
+
+for x in range(0,len(portsList)):
+    if portsList [x].startswith("COM"+ str(val)):
+        portVar = "COM" + str(val)
+        print(portVar)
+
+serialInst.baudrate=9600    
+serialInst.port = portVar
+serialInst.open()
+
+   
+while True:
+    command=input("Arduino Command : (On/Off): ")
+    serialInst.write(command.encode('utf-8'))
+    if command == 'exit':
+        exit()
+    
+    
+    
+    
