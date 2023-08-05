6734a4ae4dc859a1b1b1c19cf8bbd16b4981556e
saurav2407 <135573682+saurav2407@users.noreply.github.com>
2023-08-06 02:51:13 +0530

    Add files via upload

---
+++ alarm.py
@@ -0,0 +1,17 @@
+# import os
+# os.startfile(r"C:\Users\saura\Music\jarvis_alarm.mp3")
+# print('playing sound using playsound')
+
+
+# import required module
+from playsound import playsound 
+
+# for playing note.wav file
+playsound("C:\Users\saura\Music\piano-bell-sound-1-27144.mp3")
+
+
+
+
+
+# import playsound as ps
+# ps.playsound("C:\Users\saura\Music\jarvis_alarm.mp3")
