55c96b8c6b2511dd2f29d11ea9ce2e96d5d4f70e
saurav2407 <135573682+saurav2407@users.noreply.github.com>
2023-08-06 04:02:20 +0530

    Add files via upload

---
+++ mousereader.py
@@ -0,0 +1,22 @@
+import pyautogui as pg
+# pyautogui.click(1228, 350)
+# pyautogui.moveTo(button="right")
+ 
+# # moves to the specified location
+# # pyautogui.moveTo(1207,621, duration = 1)
+ 
+# # clicks at the present location
+# # pyautogui.click()Geeks For Geeks!
+# # # pauses the execution of the program
+# # # for 5 sec
+# time.sleep(5)
+ 
+# # # types the string passed inside the
+# # # function
+# # pyautogui.typewrite("Geeks For Geeks!")
+
+# import time
+pg.hotkey('alt')
+pg.hotkey('enter')
+pg.hotkey('enter')
+pg.typewrite('made by jarvis')
