936d872f6cc42018783de513cf35e68ee3bd486d
saurav2407 <135573682+saurav2407@users.noreply.github.com>
2023-08-06 03:52:12 +0530

    Add files via upload

---
+++ kon_banega_crorepati.py
@@ -0,0 +1,117 @@
+
+dict={'call':'calling a Friend...','50:50':'Removing Two options','public vote':'public Vote',}
+
+
+
+
+
+print('WELCOME TO CRORE PATI')
+print('you have life line')
+print(dict)
+print
+print('The 1st question coming on your screen')
+
+print('\n')
+print('who was Pm modi in 2013')
+print('(a) Narendra Modi      (b) Rahul Gandhi')
+print('(c) nerav modi          (d)ved prakash pathak')
+ans=input("Answer:")
+if ans=='a':
+    print('corrrect')
+    print('you won 3000 rupees')
+elif 'lifeline' or 'life line' in ans:
+    print('You choose lifeline')
+    print('you have three life lines')
+    print('call,50:50,public vote')
+    life=input('Choose LifeLine:')
+    if 'call' in life:
+        print('calling....')
+        print('friend: the correct answer is (a)')
+    elif '5050' or '50:50' in life:
+        print('Removing Two options from the question')
+        print('(a) Narendra         ')
+        print('(c) neerav           ')
+        an=input('enter options')
+        if an=="a":
+            print('correct answer')
+        else:
+            print('wrong answer')
+    elif 'public' in life:
+        print('calling....')
+        print('publics: the correct answer is (a)')
+else:
+    print('Worng answer')
+#second
+print('second question coming on your screen\n')
+print('who is the king of jungle')
+print('(a) lion         (b) elephant')
+print('(c) tiger          (d)Dolphin')
+anssw=input('Answer:')
+
+if anssw=="a":
+        print('corrrect')
+        print('you won  6000 rupees')
+
+elif 'lifeline' or 'life line' in anssw:
+    print('You choose lifeline')
+    print('you have three life lines')
+    print('call,50:50,public vote')
+    life=input('Choose LifeLine:')
+    if 'call' in life:
+        print('calling....')
+        print('friend: the correct answer is (a)')
+    elif '5050' or '50:50' in life:
+        print('Removing Two options from the question')
+        print('(a) lion         ')
+        print('(c) tiger          ')
+        an=input('enter options')
+        if an=="a":
+            print('correct answer')
+        else:
+            print('wrong answer')
+    elif 'public' in life:
+        print('calling....')
+        print('publics: the correct answer is (a)')
+        
+            
+            
+
+else:
+    print('Worng answer')
+print('Next question coming on your screen')
+print('where is the supreme court')
+print('(a) Punjab         (b) Raipur')
+print('(c) Indore          (d)Delhi')
+anssw=input('Answer:')
+
+if anssw=="d":
+        print('corrrect')
+        print('you won  10000 rupees')
+elif 'lifeline' or 'life line' in anssw:
+    print('You choose lifeline')
+    print('you have three life lines')
+    print('call,50:50,public vote')
+    life=input('Choose LifeLine:')
+    if 'call' in life:
+        print('calling....')
+        print('friend: the correct answer is (d)')
+    elif '5050' or '50:50' in life:
+        print('Removing Two options from the question')
+        print('         ')
+        print('(c) indore   (d) delhi       ')
+        an=input('enter options')
+        if an=="d":
+            print('correct answer')
+        else:
+            print('wrong answer')
+    elif 'public' in life:
+        print('calling....')
+        print('publics: the correct answer is (d)')
+else:
+    print('Worng answer')
+print('Next question coming on your screen')
+
+    
+
+
+    
