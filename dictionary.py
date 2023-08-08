663a0c3ae4809f6d87c772151be753b697038efd
SAURAV PATHAK <135573682+saurav2407@users.noreply.github.com>
2023-08-08 22:32:03 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,21 @@
+dict={'set':'collection of object','current':'flowing charge per second','charge':'elementry particle'}
+
+dict_={'you':{'me':"saurav",'you':'ved','he':'sundram'},'ved':'love','saksham':'smart'}
+
+
+
+# print(dict_['you']['you'])
+# print(dict['current'])
+# del dict ['set']
+# print(dict)
+# dict=dict_.copy()         #on comparing two dictionary it helps to not show false value 
+# dict=dict_.get('you')     #search the name 
+# dict_.update({'yash':'brother'})   #add inside the dictionary
+# dict_.items()
+# dict_.keys()
+
+# print(dict_)
+user=input('Search:')
+print(dict[user])
+
+    
