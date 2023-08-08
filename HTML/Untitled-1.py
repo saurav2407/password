f899b7e5934b9b31ac305cbd4ad6cde32c372ad6
SAURAV PATHAK <135573682+saurav2407@users.noreply.github.com>
2023-08-08 22:32:47 +0530

    Add files via upload

---
+++ HTML/Untitled-1.py
@@ -0,0 +1,125 @@
+import pyautogui as pg
+import time
+x=input('jnd')
+y=input('jj')
+
+while True:
+    time.sleep(0.40)
+    x = (int(x) - 50)
+    y = (int(y) - 50)
+    a=pg.mouseUp(x,y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def is_round(i):
+#     x=0
+#     y=0
+    
+    
+#     for i in i :
+        
+#         if i=='r':
+#             print(x=x+1)
+            
+#         if i=='l':
+#             print(x=x-1)
+#         if i=='u':
+#             print(y=y+1)
+#         if i=='d':
+#             print(y=y-1)
+#     return x==0 and y==0
+
+    
+# user=input('enter your path:').lower()
+
+# path=is_round(user)
+# print(path)
+# if path==True:
+#     print('you are at initial position')
+# else:
+#     print(f'you are at {path}')
+
+
+
+
+
+
+
+
